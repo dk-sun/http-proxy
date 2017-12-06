5d5adcc0640c15a6b0baa3e1004b2045fb136c28
dk-sun <dk_sun@126.com>
2017-12-06 15:53:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var ejs = require('ejs');
 var port = process.env.PORT|| 63342;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname+'/'));
+app.use(express.static(__dirname+'/src/'));
 app.set('views', __dirname + '/');
 app.engine('.html', ejs.__express)
 app.set('view engine','html');
@@ -16,6 +16,6 @@ app.get("/",function(req,res){
 	res.render('index');
 });
 //设置要代理的到的地址
-proxy.setProxy(app,'/api/weather','http://op.juhe.cn/onebox/weather/query');
+proxy.setProxy(app,'/','http://192.168.100.90/api');
 http.createServer(app).listen(port);
 console.log("正在监听%d端口...",port);
